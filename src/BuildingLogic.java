ea13f7b64a582f98daf9ac50a85f99f9b90f4164
pacific23 <48202212+pacific23@users.noreply.github.com>
2021-06-30 22:47:33 +0200

    MAJ

--- src/BuildingLogic.java
+++ src/BuildingLogic.java
@@ -4,7 +4,7 @@ import Enum.ContrainteLevel;
 
 public class BuildingLogic {
     public static void main(String[] args){
-        Probleme probleme =new Probleme(Arrays.asList(ContrainteLevel.PIECE_PRESENTE_I4, ContrainteLevel.PIECE_PRESENTE_T, ContrainteLevel.PIECE_PRESENTE_O, ContrainteLevel.PIECE_PRESENTE_L, ContrainteLevel.PIECE_PRESENTE_Z, ContrainteLevel.PIECE_PRESENTE_J, ContrainteLevel.PIECE_PRESENTE_S, ContrainteLevel.PIECE_PRESENTE_V, ContrainteLevel.NB_PIECES_PRESENTE_8));
+        Probleme probleme =new Probleme(Arrays.asList(ContrainteLevel.PIECE_PRESENTE_I4, ContrainteLevel.PIECE_PRESENTE_T, ContrainteLevel.PIECE_PRESENTE_O, ContrainteLevel.NB_PIECES_PRESENTE_3));
 //        probleme.export("");
         probleme.exportGraphical("G_");
         Solveur solveur = new Solveur(probleme);
